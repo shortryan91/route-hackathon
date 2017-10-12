2bce9eed151bbb9d146df3eb3c15f9758897f24c
Ryan Short <rcshort9376@hotmail.com>
2017-10-12 20:29:12 +1100

    firt commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+
+  get '/pages/about', to: 'pages#about'
+  
+  resources :pages
+
+
+  resources :maps
+end
